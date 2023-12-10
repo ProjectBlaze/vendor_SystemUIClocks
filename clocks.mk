c3096f126d21d63ab5d3fc43b68f8c811237bf84
RAM-UNLOK <rakmoparte@gmail.com>
2023-12-10 14:56:32 +0530

    clocks: Initialize SystemUIClocks from husky-ota-uq1a.231205.015
    Add SystemUIClocks-BigNum
    Add SystemUIClocks-Calligraphy
    Add SystemUIClocks-Flex
    Add SystemUIClocks-Growth
    Add SystemUIClocks-Inflate
    Add SystemUIClocks-NumOverlap
    Add SystemUIClocks-Weather
    Add SystemUIClocks-Metro
    
    Signed-off-by: RAM-UNLOK <rakmoparte@gmail.com>

---
+++ clocks.mk
@@ -0,0 +1,13 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/SystemUIClocks
+
+# Clocks
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap \
+    SystemUIClocks-Weather
